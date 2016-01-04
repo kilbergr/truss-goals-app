7111746bb2465f571a552ca408a263167f3a12ab
Breanne Boland <breanneboland@gmail.com>
2016-01-04 15:18:06 -0800

    draft goal creation page

--- Truss Goals App/ViewController.swift
+++ Truss Goals App/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        // Do any additional setup after loading the view, typically from a 
+        }
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 7
+        }
+    
 
+    
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+            
+            cell.textLabel?.text = "Sample skill"
+            return cell
+        }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
