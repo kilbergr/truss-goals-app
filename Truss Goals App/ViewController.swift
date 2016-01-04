f0ede0fdcca17d57c04c37b2ae1891ff6424fc2e
Breanne Boland <breanneboland@gmail.com>
2016-01-04 13:48:20 -0800

    Initial Commit

---
+++ Truss Goals App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Truss Goals App
+//
+//  Created by Breanne Boland on 1/4/16.
+//  Copyright © 2016 Breanne Boland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
